50d9e2e3869ce2f917ee215ad49198397692dd43
Enrique Hernández Bello <quique@osl.ull.es>
2011-02-16 21:20:38 +0000

    bardinux-sigaull-default-settings:     - Added configuration to reduze Mozilla Firefox disk cache on clients.     - Choosed an alternative way to apply this profile on kde.     - Lockdown default wallpaper in guest mode.

---
+++ bardinux-sigaull-default-settings/gcs/newfiles_skel/usr/share/firefox/defaults/pref/sigaull.js
@@ -0,0 +1,4 @@
+// This is the SIGAULL specific preferences file for Mozilla Firefox
+
+// Reduces the disk cache capacity to avoid to fill the disk quota
+pref("browser.cache.disk.capacity", 5120);
