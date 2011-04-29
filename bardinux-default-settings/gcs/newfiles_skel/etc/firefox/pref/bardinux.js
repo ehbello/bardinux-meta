c7196908a143415ce9ab3cd6a4bf965fe25f4e23
Enrique Hernández Bello <quique@osl.ull.es>
2011-04-29 18:14:04 +0100

    bardinux-default-settings:   - Fixed configuration to set the default firefox homepage.

--- bardinux-default-settings/gcs/newfiles_skel/etc/firefox/pref/bardinux.js
+++ bardinux-default-settings/gcs/newfiles_skel/etc/firefox/pref/bardinux.js
@@ -1,2 +1,1 @@
-user_pref("browser.startup.homepage", "http://bardinux.ull.es/");
-user_pref("browser.startup.homepage_override.mstone", "ignore");
+pref("browser.startup.homepage_override.mstone", "ignore");
