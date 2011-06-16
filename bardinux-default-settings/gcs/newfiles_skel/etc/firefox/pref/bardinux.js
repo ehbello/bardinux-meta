1205ad21269d74fbd37da2eaf87a3ab895765ae2
Enrique Hernández Bello <quique@osl.ull.es>
2011-06-16 16:35:06 +0100

    bardinux-default-settings:   - Set A4 as default paper size for all printers insetad of US Letter.

--- bardinux-default-settings/gcs/newfiles_skel/etc/firefox/pref/bardinux.js
+++ bardinux-default-settings/gcs/newfiles_skel/etc/firefox/pref/bardinux.js
@@ -1,1 +1,10 @@
 pref("browser.startup.homepage_override.mstone", "ignore");
+
+// Set A4 as default paper size for all printers insetad of US Letter.
+pref("print.postcript.paper_size", "a4");
+pref("print.print_paper_height", "297,00");
+pref("print.print_paper_name", "iso_a4");
+pref("print.print_paper_size", 3);
+pref("print.print_paper_size_type", 1);
+pref("print.print_paper_size_unit", 1);
+pref("print.print_paper_width", "210,00");
