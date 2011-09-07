c8914010e990771c6fdd48b444444c3aed3782d7
Enrique Hernández Bello <ehbello@gmail.com>
2011-09-07 21:31:50 +0100

    bardinux-system-restore-tool:   - Bunch of changes to get work this system with grub2.

---
+++ bardinux-system-restore-tool/gcs/install_scripts/pos/enable-grub.d
@@ -0,0 +1,4 @@
+#chmod -f +x /etc/grub.d/1*
+#chmod -f +x /etc/grub.d/2*
+#chmod -f +x /etc/grub.d/3*
+#chmod -f +x /etc/grub.d/4*
---
+++ bardinux-system-restore-tool/gcs/remove_scripts/pos/enable-grub.d
@@ -0,0 +1,4 @@
+chmod -f +x /etc/grub.d/10_linux
+chmod -f +x /etc/grub.d/20_memtest86+
+chmod -f +x /etc/grub.d/30_os-prober
+chmod -f +x /etc/grub.d/40_custom
