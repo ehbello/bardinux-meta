4f8d4a528d053a4ef0cdcb27d471ff590b178551
Enrique Hernández Bello <quique@osl.ull.es>
2013-06-14 14:02:31 +0100

    Released packages:     - bardinux-default-settings (1.3.16) bardinux-first-start: (New version: 1.3.4)     - Fixed compatibility of this application with Firefox disabling MaxVersion.
    xul-conf-bardinux:
    (New version: 1.3.2)
        - Fixed AptUrl configuration for Firefox.

--- xul-conf-bardinux/gcs/newfiles_skel/usr/lib/firefox/browser/defaults/preferences/apturl.js
+++ xul-conf-bardinux/gcs/newfiles_skel/usr/lib/firefox/browser/defaults/preferences/apturl.js
@@ -1,6 +1,1 @@
-pref("network.protocol-handler.app.apt","/usr/bin/apturl");
-pref("network.protocol-handler.warn-external.apt",false);
-pref("network.protocol-handler.app.apt+http","/usr/bin/apturl");
-pref("network.protocol-handler.warn-external.apt+http",false);
-pref("network.protocol-handler.external.apt",true);
-pref("network.protocol-handler.external.apt+http",true);
+pref("network.protocol-handler.expose.apt",false);
